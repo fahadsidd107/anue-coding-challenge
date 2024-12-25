d1d3df3602868746b9be007e3e98225b8b346d05
fahadsidd107 <fsiddiqui107@gmail.com>
2024-12-25 04:01:39 +0100

    fix: eslint new config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,30 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+/** @type {import('eslint').Linter.Config} */
+export default {
+  root: true,
+  extends: [
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.recommended
+  ],
+  ignorePatterns: ["**/*.js", "**/*.mjs", "**/*.cjs", "**/*.jsx"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+};
