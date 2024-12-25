35f41628b368d9520172327cd89ccc7ece19aefd
fahadsidd107 <fsiddiqui107@gmail.com>
2024-12-25 04:11:07 +0100

    test: ssr fix test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Theme from "../Theme/Theme";
 import TaskListContainer from "../ToDoList/TaskListContainer";
 import ToastContainerWrapper from "@/components/Toast/ToastContainerWrapper";
 
 const Page: React.FC = () => {
+  useEffect(() => {}, []);
   return (
     <Theme>
       <Navbar />
